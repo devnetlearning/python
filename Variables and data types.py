a570f324ddf59c5a6b3020922c69665b07a81ea7
devnetlearning <szklarjakub@gmail.com>
2020-12-13 14:54:50 +0100

    First part of python basics teory

---
+++ Variables and data types.py
@@ -0,0 +1,60 @@
+"""
+Variable - label mapped to the object.
+Rules in assigning variables:
+- Must start with letter or underscore character
+- Can't strt with number.
+- Name is case sensitive.
+"""
+#example
+
+name = "mark" # Variable assigned to string
+age = 9 # Variable assigned to integer
+married = True #Variable assigned to Boolean
+full name = name + " Smith" #Variable assigned to another variable
+
+
+#Data types:
+"""
+1.Mutable - allowed to be changed(Objects that will be edited):
+- List - Ordered sequence of Objects
+- Dictionary - unordered key:value pairs (explained later)
+- Set - unordered group of unique objects
+"""
+
+
+
+
+"""
+2.Immutable - not allowed to be changed (Constant values that you don't want
+to be changed)
+
+- String - group of ordered characters ("Test" , "Mark" , "2020"):
+- Integer - number without decimal points ( 1 , 10 , 23):
+
+ Build in operations for working with numbers:
+
+ + - add
+ - - subtract
+ * - multiply
+ / - divide
+ // - integer division
+ % - modulus
+ ** - exponent
+
+- Float - number with decimal points (1.2 , 13.4 , 565.1312)
+
+- Boolean - Compariasion value (True or False):
+
+ Compariasion operations:
+
+< - Less than
+> - Greater than
+<= Less than or equal
+>= Greater than or equal
+== equal
+!= not equal
+
+- Tuple - Orderer group of immutable objects:
+
+
+"""
