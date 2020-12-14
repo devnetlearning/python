54aae737b48cdeab734980c6884cac85eb27652d
devnetlearning <szklarjakub@gmail.com>
2020-12-14 12:00:41 +0100

    Finished variables and data types

--- Variables and data types.py
+++ Variables and data types.py
@@ -13,25 +13,42 @@ married = True #Variable assigned to Boolean
 full name = name + " Smith" #Variable assigned to another variable
 
 
-#Data types:
+#Data types and operations:
 """
-1.Mutable - allowed to be changed(Objects that will be edited):
-- List - Ordered sequence of Objects
-- Dictionary - unordered key:value pairs (explained later)
-- Set - unordered group of unique objects
-"""
+1.Mutable - allowed to be changed (Objects that will be edited):
 
+- List - Ordered sequence of Objects:
+Can contain integers, strings, and even other lists.
+test = [Mark, Clark, James]
 
+- Dictionary - unordered key:value pairs:
+build in system for storing data as key:value pairs. Assigning data to
+the key and using it later. A value can be any Python object.
 
+- Set - unordered group of unique objects:
 
-"""
+test = {1,3,5,7,8,9,0,1,2}
+to join two sets without duplicates you should use pipe  "|"
+
 2.Immutable - not allowed to be changed (Constant values that you don't want
 to be changed)
 
 - String - group of ordered characters ("Test" , "Mark" , "2020"):
-- Integer - number without decimal points ( 1 , 10 , 23):
 
- Build in operations for working with numbers:
+    Even if string contain number it's still considered as character
+
+    int("10") - convert string to integer
+
+- Integer - number without decimal points ( 1 , 10 , 23)
+
+- Float - number with decimal points (1.2 , 13.4 , 565.1312)
+
+- Boolean - Compariasion value (True or False)
+
+- Tuple - Orderer group of immutable objects:
+test = ( 12 , Amy , student )
+
+Build in operations for working with numbers:
 
  + - add
  - - subtract
@@ -41,11 +58,7 @@ to be changed)
  % - modulus
  ** - exponent
 
-- Float - number with decimal points (1.2 , 13.4 , 565.1312)
-
-- Boolean - Compariasion value (True or False):
-
- Compariasion operations:
+Compariasion operations:
 
 < - Less than
 > - Greater than
@@ -53,8 +66,4 @@ to be changed)
 >= Greater than or equal
 == equal
 != not equal
-
-- Tuple - Orderer group of immutable objects:
-
-
 """
